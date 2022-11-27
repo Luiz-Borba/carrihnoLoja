335e3ddb2dfd002f2748497b1eaa293505c12cff
Luiz Felipe Prado Borba <luis.p.borba@hotmail.com>
2022-11-27 13:07:29 -0300

    ajuste transição de imagens

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ class Products{
 
     trocaImagem1(){
         document.getElementById("produto1").src ="./images/image-product-1.jpg";
+        document.getElementById("1").style.transition = "all 0.5s"
         document.getElementById("1").style.opacity = "0.5";
         document.getElementById("2").style.opacity = "1";
         document.getElementById("3").style.opacity = "1";
@@ -9,6 +10,7 @@ class Products{
     }
     trocaImagem2(){
         document.getElementById("produto1").src ="./images/image-product-2.jpg";
+        document.getElementById("2").style.transition = "all 0.5s"
         document.getElementById("2").style.opacity = "0.5";
         document.getElementById("1").style.opacity = "1";
         document.getElementById("3").style.opacity = "1";
@@ -17,6 +19,7 @@ class Products{
     trocaImagem3(){
         document.getElementById("produto1").src ="./images/image-product-3.jpg";
         document.getElementById("3").style.opacity = "0.5";
+        document.getElementById("3").style.transition = "all 0.5s"
         document.getElementById("2").style.opacity = "1";
         document.getElementById("1").style.opacity = "1";
         document.getElementById("4").style.opacity = "1";
@@ -24,6 +27,7 @@ class Products{
     trocaImagem4(){
         document.getElementById("produto1").src ="./images/image-product-4.jpg";
         document.getElementById("4").style.opacity = "0.5";
+        document.getElementById("4").style.transition = "all 0.5s"
         document.getElementById("2").style.opacity = "1";
         document.getElementById("3").style.opacity = "1";
         document.getElementById("1").style.opacity = "1";
