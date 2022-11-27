e72c3838f79bd2edcacbead1801fd6d7d0e9dc1b
Luiz Felipe Prado Borba <luis.p.borba@hotmail.com>
2022-11-27 12:23:29 -0300

    trocar imagem do protudo

---
+++ index.js
@@ -0,0 +1,34 @@
+class Products{
+
+    trocaImagem1(){
+        document.getElementById("produto1").src ="./images/image-product-1.jpg";
+        document.getElementById("1").style.opacity = "0.5";
+        document.getElementById("2").style.opacity = "1";
+        document.getElementById("3").style.opacity = "1";
+        document.getElementById("4").style.opacity = "1";
+    }
+    trocaImagem2(){
+        document.getElementById("produto1").src ="./images/image-product-2.jpg";
+        document.getElementById("2").style.opacity = "0.5";
+        document.getElementById("1").style.opacity = "1";
+        document.getElementById("3").style.opacity = "1";
+        document.getElementById("4").style.opacity = "1";
+    }
+    trocaImagem3(){
+        document.getElementById("produto1").src ="./images/image-product-3.jpg";
+        document.getElementById("3").style.opacity = "0.5";
+        document.getElementById("2").style.opacity = "1";
+        document.getElementById("1").style.opacity = "1";
+        document.getElementById("4").style.opacity = "1";
+    }
+    trocaImagem4(){
+        document.getElementById("produto1").src ="./images/image-product-4.jpg";
+        document.getElementById("4").style.opacity = "0.5";
+        document.getElementById("2").style.opacity = "1";
+        document.getElementById("3").style.opacity = "1";
+        document.getElementById("1").style.opacity = "1";
+        
+    }
+}
+
+let trocar = new Products;
