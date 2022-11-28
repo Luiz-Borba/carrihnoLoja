468e7971d5fa8cdad0277b943240cb5b036c2f9e
Luiz Felipe Prado Borba <luis.p.borba@hotmail.com>
2022-11-27 22:46:17 -0300

    implementação do carroesel

--- index.js
+++ index.js
@@ -33,7 +33,51 @@ class Products{
         document.getElementById("1").style.opacity = "1";
         
     }
-    
+    trocaImagemItem1(){
+        document.getElementById("produto01").src ="./images/image-product-1.jpg";
+        document.getElementById("item1").style.transition = "all 0.5s"
+        document.getElementById("item1").style.opacity = "0.5";
+        document.getElementById("item2").style.opacity = "1";
+        document.getElementById("item3").style.opacity = "1";
+        document.getElementById("item4").style.opacity = "1";
+    }
+    trocaImagemItem2(){
+        document.getElementById("produto01").src ="./images/image-product-2.jpg";
+        document.getElementById("item2").style.transition = "all 0.5s"
+        document.getElementById("item2").style.opacity = "0.5";
+        document.getElementById("item1").style.opacity = "1";
+        document.getElementById("item3").style.opacity = "1";
+        document.getElementById("item4").style.opacity = "1";
+    }
+    trocaImagemItem3(){
+        document.getElementById("produto01").src ="./images/image-product-3.jpg";
+        document.getElementById("item3").style.opacity = "0.5";
+        document.getElementById("item3").style.transition = "all 0.5s"
+        document.getElementById("item2").style.opacity = "1";
+        document.getElementById("item1").style.opacity = "1";
+        document.getElementById("item4").style.opacity = "1";
+    }
+    trocaImagemItem4(){
+        document.getElementById("produto01").src ="./images/image-product-4.jpg";
+        document.getElementById("item4").style.opacity = "0.5";
+        document.getElementById("item4").style.transition = "all 0.5s"
+        document.getElementById("item2").style.opacity = "1";
+        document.getElementById("item3").style.opacity = "1";
+        document.getElementById("item1").style.opacity = "1";
+        
+    }
+    abriCarrossel(){
+    document.getElementById("carrosel").style.visibility ="visible"
+    document.getElementById("carrosel").style.zIndex ="99"
+    document.getElementById("headerContent").style.opacity ="0.3"
+    document.getElementById("container").style.opacity ="0.3"
+}
+fecharCarroseel(){
+    document.getElementById("carrosel").style.visibility ="hidden"
+    document.getElementById("carrosel").style.zIndex ="-99"
+    document.getElementById("headerContent").style.opacity ="1"
+    document.getElementById("container").style.opacity ="1"
+}
 }
 
 let trocar = new Products;
